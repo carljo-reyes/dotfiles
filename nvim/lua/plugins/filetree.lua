0d3c5057caa15563aa31021bd6e6fdeb08d66837
Carljo Reyes <carljo.reyes@cambridge.org>
2023-08-16 22:49:14 +0800

    make explorer show float by default

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -1,17 +1,31 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = true,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
     },
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        vim.opt.termguicolors = true
-    end,
+    opts = {
+        close_if_last_window = true,
+        window = {
+            position = "float",
+        },
+        filesystem = {
+            group_empty_dirs = true,
+            window = {
+                fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+                    ["<M-j>"] = "move_cursor_down",
+                    ["<M-k>"] = "move_cursor_up",
+                },
+            }
+        }
+    },
     keys = {
-        {"<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer"}
+        {"<M-e>", "<cmd>Neotree toggle reveal<cr>", desc = "Explorer"},
+        {"<leader>ef", "<cmd>Neotree toggle float<cr>", desc = "Explorer"},
+        {"<leader>eh", "<cmd>Neotree toggle left<cr>", desc = "Explorer"},
+        {"<leader>el", "<cmd>Neotree toggle right<cr>", desc = "Explorer"},
     }
 }

f549589f07ddcbba5fe25cad94bfb90a257d2b74
Carljo Reyes <carljo.reyes@cambridge.org>
2023-08-23 16:55:29 +0800

    window navigation keymaps

--- nvim/plugin/vim_keymaps.lua
+++ nvim/plugin/vim_keymaps.lua
@@ -10,9 +10,9 @@ setn("<M-l>", "<cmd>Lazy<cr>");
 setn("<M-q>", "<cmd>bd<cr>");
 setn("<M-Q>", "<cmd>qa!<cr>");
 
-setn("<C-h>", "<cmd>wincmd h<cr>");
-setn("<C-j>", "<cmd>wincmd j<cr>");
-setn("<C-k>", "<cmd>wincmd k<cr>");
-setn("<C-l>", "<cmd>wincmd l<cr>");
+setn("<M-h>", "<cmd>wincmd h<cr>");
+setn("<M-j>", "<cmd>wincmd j<cr>");
+setn("<M-k>", "<cmd>wincmd k<cr>");
+setn("<M-l>", "<cmd>wincmd l<cr>");
 
 setv("<C-c>", '"+y'); -- copy to clipboard

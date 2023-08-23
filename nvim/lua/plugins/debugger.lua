bb4eca9e7a77df387a63a2a665a5c8a339a493f6
Carljo Reyes <carljo.reyes@cambridge.org>
2023-08-23 16:50:20 +0800

    debugger keymaps

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -3,17 +3,11 @@ return {
     dependencies = {{ "mfussenegger/nvim-dap" }},
     config = true,
     keys = {
-        { "<leader>dt", function() require('dapui').toggle() end },
-        { "<leader>D", function() require('dap').toggle_breakpoint() end },
-        { "<leader>dp", function() require('dap').continue() end },
-        { "<leader>dd", function() require('dap').step_over() end },
-        { "<leader>dg", function() require('dap').step_into() end },
-        { "<leader>dr", function() require('dap').repl.open() end },
-        { "<leader>;m",
-            function()
-                require('jdtls').setup_dap()
-                require('dap').repl.open()
-            end
-        },
+        { "<leader>d", function() require('dapui').toggle() end },
+        { "<M-y>", function() require('dap').toggle_breakpoint() end },
+        { "<M-u>", function() require('dap').step_over() end },
+        { "<M-i>", function() require('dap').step_into() end },
+        { "<M-o>", function() require('dap').step_out() end },
+        { "<M-p>", function() require('dap').continue() end },
     }
 }
